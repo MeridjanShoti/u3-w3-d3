2e850ed3372281bd89d64c9ec3a3ce1d2ed16acb
MeridjanShoti <meridjanshoti@gmail.com>
2025-02-19 17:00:41 +0100

    a

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
-/* 1) Quali sono i tipi primitivi principali in TypeScript?
-2) Crea tre variabili tipizzate: una stringa con il tuo nome, un numero con la tua età, e un booleano che indica se stai studiando TypeScript.
-3) Tipizza il parametro della seguente funzione:
-const greet = (name) => { return "Ciao " + name }
-4) Specifica il tipo di ritorno della seguente funzione:
-const sum = (a: number, b: number) => { return a + b }
-5) Crea una funzione che accetti un prezzo e restituisca il prezzo con IVA (22%). Usa i tipi appropriati.
-6) Crea una funzione che concateni due stringhe e restituisca la lunghezza totale.
-7) Cos'è un Type Union e come si scrive?
-8) Crea una variabile che possa contenere un numero, null o undefined.
-9) Crea un tipo per rappresentare i giorni della settimana usando union di stringhe letterali.
-10) Tipizza il seguente array di numeri nei due modi possibili:
-const numbers = [1, 2, 3]
-11) Crea una tupla per definire un array di 5 elementi, i primi 3 devono essere stringhe e gli ultimi due numeri.
-12) Qual è la differenza tra type e interface?
-13) Definisci un'interfaccia in TypeScript per un oggetto dotato di proprietà "firstname", "lastname", e "age".
-14) Crea un'interfaccia per un utente con email obbligatoria e telefono opzionale.
-15) Crea un array tipizzato di oggetti "Studente" con nome e voto.
-16) Crea un'interfaccia base "Veicolo" e estendila per creare "Auto".
+/*
 17) Crea un oggetto che implementi l'interfaccia Auto.
 18) Cosa sono i Generics in TypeScript?
 19) È possibile avere più tipi generici in un'interfaccia?
@@ -92,3 +74,25 @@ var meridjan = {
 console.log("type of " + meridjan.firstname, typeof meridjan.firstname);
 console.log("type of " + meridjan.lastname, typeof meridjan.lastname);
 console.log("type of " + meridjan.age, typeof meridjan.age);
+console.log("14) Crea un'interfaccia per un utente con email obbligatoria e telefono opzionale.");
+var temistualdo = {
+    name: "temistualdo",
+    email: "temistualdo@vdm.it",
+};
+console.log("type of ".concat(temistualdo.name), typeof temistualdo.name);
+console.log("type of ".concat(temistualdo.email), typeof temistualdo.email);
+console.log('15) Crea un array tipizzato di oggetti "Studente" con nome e voto.');
+var studente1 = { nome: "Mario", voto: 7 };
+var studente2 = { nome: "Luigi", voto: 5 };
+var studente3 = { nome: "Giangianni", voto: 10 };
+var studentArray;
+studentArray = [studente1, studente2, studente3];
+console.log(studentArray);
+console.log("16) Crea un'interfaccia base \"Veicolo\" e estendila per creare \"Auto\".");
+var yaris = {
+    ruote: 4,
+    alimentazione: "ibrido",
+    cambio: "automatico",
+    kilometri: 200000,
+    marca: "toyota",
+};
